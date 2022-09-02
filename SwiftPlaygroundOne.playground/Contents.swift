b08bb615903498066278a1e3baddc6e5166221f6
mehranHAWAII <112056410+mehranHAWAII@users.noreply.github.com>
2022-09-01 16:46:50 -1000

    Completed Assignment #2 Video
    Boolean Variables which are known as True or False

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -13,5 +13,17 @@ var mySecondVariable = "and professor look that I added two strings together"
 var myThiredVariable = myFirstVariable + mySecondVariable
 print(myThiredVariable)
 
+/// Assignment #2 Introduction to True or False or Boolean Variables
+var appState = false
+print(appState)
+
+//Now lets comaparing my variables using > (greater than) or < (less than)
+var myNumberOne = 4
+var myNumberTwo = 14
+
+//At the end I need to create a variable that can holds the comparison on my two variables that I have just created in the above
+var compare = myNumberOne > myNumberTwo
+
+
 
 
