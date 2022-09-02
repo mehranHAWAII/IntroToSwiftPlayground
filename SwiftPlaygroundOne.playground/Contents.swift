c1373a80500c21a79904e7da75b340250da0818a
mehranHAWAII <112056410+mehranHAWAII@users.noreply.github.com>
2022-09-02 08:41:49 -1000

    Part 7 Assignment #2 Completed

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -14,15 +14,29 @@ var myThiredVariable = myFirstVariable + mySecondVariable
 print(myThiredVariable)
 
 /// Assignment #2 Introduction to True or False or Boolean Variables
-var appState = false
-print(appState)
+//var appState = false
+//print(appState)
 
 //Now lets comaparing my variables using > (greater than) or < (less than)
-var myNumberOne = 4
-var myNumberTwo = 14
+//var myNumberOne = 4
+//var myNumberTwo = 14
 
 //At the end I need to create a variable that can holds the comparison on my two variables that I have just created in the above
-var compare = myNumberOne > myNumberTwo
+//var compare = myNumberOne > myNumberTwo
+
+//Part 7 Using the equal to Operator
+var myState = true
+print(myState)
+//var bestState: String
+var bestState = "Aloha State"
+
+var beautifullestState : String
+beautifullestState = "Aloha State"
+
+myState = bestState == beautifullestState
+print (myState)
+
+
 
 
 
