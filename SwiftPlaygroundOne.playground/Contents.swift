3072e5a24128d189a4b5b0f18fb781c035df1d50
mehranHAWAII <112056410+mehranHAWAII@users.noreply.github.com>
2022-09-01 16:05:10 -1000

    Finished Swift Playground One Assignment

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -2,5 +2,16 @@ import UIKit
 
 var str = "Hello, playground"
 //my first comment just start
-// this is my actual playgroound file 
+// this is my actual playgroound file
+
+var myFirstVariable: String
+myFirstVariable = "I've created my first variable with Swift"
+
+//we're going to do something with strings
+var mySecondVariable = "and professor look that I added two strings together"
+
+var myThiredVariable = myFirstVariable + mySecondVariable
+print(myThiredVariable)
+
+
 
